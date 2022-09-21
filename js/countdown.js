15c1fa7bf5975551d528e939cf682ae19ec8b42e
RachelJiarui <81569164+RachelJiarui@users.noreply.github.com>
2022-09-21 16:01:46 -0400

    Reupload files

---
+++ js/countdown.js
@@ -0,0 +1,25 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Oct 21, 2022").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown-date").innerHTML = days + "d " + hours + "h ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown-date").innerHTML = "Input a new date";
+  }
+}, 1000);
